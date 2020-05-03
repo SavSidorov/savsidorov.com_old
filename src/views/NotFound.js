1c1169550f22f6b6288a584e458b7ef2492cf7e1
ssidorov <sav01027@gmail.com>
2020-05-03 00:48:27 -0600

    Add NotFound formatting

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,11 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-//TODO: Formatting
+const Styles = styled.div`
+	.notfound {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+`;
 
 export default function Contact() {
 	return (
-		<div>
-			<h2>Sorry, this page doesn't exist.</h2>
-		</div>
+		<Styles>
+			<div className="notfound">
+				<h2>Sorry, this page doesn't exist.</h2>
+			</div>
+		</Styles>
 	);
 }
