d07cc5b844d1018b3c5277046340e59490912e06
ssidorov <sav01027@gmail.com>
2020-05-02 23:28:30 -0600

    Implement FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
+//React Router
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+//FontAwesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+//Views
 import Home from "./views/Home";
 import Blog from "./views/Blog";
 import Projects from "./views/Projects";
 import Favorites from "./views/Favorites";
 import Contact from "./views/Contact";
 import NotFound from "./views/NotFound";
-
+//Components
 import { Header } from "./components/Header";
 import { NavBar } from "./components/NavBar";
 import { Layout } from "./components/Layout";
 import { Footer } from "./components/Footer";
+
+library.add(fab, faCoffee);
 
 function Routes() {
 	return (
