69ac23db0f58d18c82f516e7d63afdddc8414b77
Sav Sidorov <sav01027@gmail.com>
2021-11-24 14:42:43 -0800

    Updated Library, again

--- src/views/library/Talks.js
+++ src/views/library/Talks.js
@@ -20,13 +20,14 @@ export default function Talks() {
 	return (
 		<Styles>
       <Navigation/>
-			<p>Talks & lectures that I frequently refer back to...</p>
+			<p>Talks & lectures that I frequently come back to...</p>
 			<br/>
 			<div className="Talks">
 				<iframe title="Bob Dylan Nobel Lecture" width="400" height="225" src="https://www.youtube.com/embed/6TlcPRlau2Q" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<iframe title="Nassim Taleb Commencement" width="400" height="225" src="https://www.youtube.com/embed/Fzmr3urk28I" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<iframe title="Peter Thiel Commencement" width="400" height="225" src="https://www.youtube.com/embed/id4ywg5oemc" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<iframe title="Steve Jobs Commencement" width="400" height="225" src="https://www.youtube.com/embed/UF8uR6Z6KLc" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+				<iframe title="Kevin Spacey" width="400" height="225" src="https://www.youtube.com/embed/oheDqofa5NM" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<iframe title="Karthik TED Talk" width="400" height="225" src="https://www.youtube.com/embed/MfC3JdkEVgQ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<iframe title="Effective FIRST Strategies 2019, part 1" width="400" height="225" src="https://www.youtube.com/embed/5fifL47TvzE" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<iframe title="The Humane Representation of Thought" width="400" height="225" src="https://www.youtube.com/embed/agOdP2Bmieg" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
