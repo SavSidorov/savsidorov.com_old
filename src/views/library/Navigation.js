d3b4bc417918c1967cdf49f3426fafcff187dc99
Sav <sav01027@gmail.com>
2022-04-15 23:23:26 -0700

    Removed people section

--- src/views/library/Navigation.js
+++ src/views/library/Navigation.js
@@ -1,45 +1,55 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Styles = styled.div`
-.library-nav{
-  display: flex;
-  margin-bottom: 20px;
-}
+	.library-nav {
+		display: flex;
+		margin-bottom: 20px;
+	}
 
-h3 > a {
-  font-size: 25px;
-  color: gray;
-  text-decoration: none;
+	h3 > a {
+		font-size: 25px;
+		color: gray;
+		text-decoration: none;
 
-  &:hover {
-    color: var(--ss-primary);
-    text-decoration: none;
-  }  
-}
+		&:hover {
+			color: var(--ss-primary);
+			text-decoration: none;
+		}
+	}
 
-.selected {
-	color: black;
+	.selected {
+		color: black;
 
-  &:hover {
-    color: black;
-  }  
-}
-
-
-
+		&:hover {
+			color: black;
+		}
+	}
 `;
 
 export default function Navigation() {
 	return (
 		<Styles>
-      <div className="library-nav">
-        <h3><NavLink to="/library/quotes" activeClassName="selected" >Quotes</NavLink>/</h3>
-        <h3><NavLink to="/library/writings" activeClassName="selected" >Writings</NavLink>/</h3>
-        <h3><NavLink to="/library/talks" activeClassName="selected">Talks</NavLink>/</h3>
-        <h3><NavLink to="/library/people" activeClassName="selected">People</NavLink></h3>
-      </div>
+			<div className="library-nav">
+				<h3>
+					<NavLink to="/library/quotes" activeClassName="selected">
+						Quotes
+					</NavLink>
+					/
+				</h3>
+				<h3>
+					<NavLink to="/library/writings" activeClassName="selected">
+						Writings
+					</NavLink>
+					/
+				</h3>
+				<h3>
+					<NavLink to="/library/talks" activeClassName="selected">
+						Talks
+					</NavLink>
+				</h3>
+			</div>
 		</Styles>
 	);
 }
